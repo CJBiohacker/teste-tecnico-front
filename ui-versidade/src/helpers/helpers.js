bcffd1b59c15533d879e734becbc8227770c3b6a
Carlos de Lima Junior <cj.moyses@gmail.com>
2024-07-21 02:17:07 -0300

    feat: create helpers functions

---
+++ ui-versidade/src/helpers/helpers.js
@@ -0,0 +1,10 @@
+const helpers = {}
+
+helpers.getTrueKeys = (obj) => {
+  const allowedKeys = ['xxl','xl', 'lg', 'md', 'sm', 'xs']
+  const result = Object.keys(obj).filter((key) => allowedKeys.includes(key) && obj[key])
+
+  return result
+}
+
+export default helpers
