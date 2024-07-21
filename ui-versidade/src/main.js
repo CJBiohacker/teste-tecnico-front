433887326a0714b6a25ffb272365018a276bef43
Carlos de Lima Junior <cj.moyses@gmail.com>
2024-07-20 21:59:36 -0300

    feat: add pinia store package

--- ui-versidade/src/main.js
+++ ui-versidade/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
@@ -6,6 +7,7 @@ import vuetify from './plugins/vuetify';
 import 'vuetify/styles';
 import './styles/base.css';
 
+const pinia = createPinia();
 const app = createApp(App);
-app.use(router).use(vuetify);
+app.use(pinia).use(router).use(vuetify);
 app.mount('#app');
