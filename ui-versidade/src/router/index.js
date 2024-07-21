f3643d6509e5017ae91f2549546c82ed43ede0ea
Carlos de Lima Junior <cj.moyses@gmail.com>
2024-07-21 18:15:52 -0300

    feat: add Home Welcome view route

--- ui-versidade/src/router/index.js
+++ ui-versidade/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import UserCards from '@/components/UserCards.vue';
+import HomeWelcome from '@/views/HomeWelcome.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => HomeView
+      component: () => HomeWelcome
     },
     {
       path: '/users',
