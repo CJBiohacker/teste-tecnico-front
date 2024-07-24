4c99cfc6684b5569e36c61caec9975f504f7253d
Carlos de Lima Junior <cj.moyses@gmail.com>
2024-07-24 02:34:47 -0300

    feat: improve the User Cards list component with search and pagination

--- ui-versidade/src/components/UserCards.vue
+++ ui-versidade/src/components/UserCards.vue
@@ -1,5 +1,5 @@
 <script>
-import { computed } from 'vue';;
+import { computed, ref } from 'vue';;
 import { useDisplayStore } from '@/stores/display';
 import { mocks } from '@/helpers/mocks';
 import helpers from '@/helpers/helpers';
@@ -9,11 +9,11 @@ export default {
     // Substituir os mocks pelo retorno da requisição de Get (List Users) da API
     const { listedUsers } = mocks;
     const users = listedUsers.data;
+    const search = ref('');
+    const { displayValues } = useDisplayStore();
 
     const responsiveCols = computed(() => {
       let numCols;
-      const { displayValues } = useDisplayStore();
-      const { width } = displayValues;
 
       const responsiveColValues = {
         xxl: 2,
@@ -28,54 +28,136 @@ export default {
       numCols = trueKeys.map(key => responsiveColValues[key]).join('');
 
       if (trueKeys.join('') === 'md') {
-        numCols = width >= 960 && width < 1280 ? 6 : 4;
+        numCols = displayValues.width >= 960 && displayValues.width < 1280 ? 6 : 4;
       }
 
       return numCols;
     });
 
+    const responsiveSearchBar = computed(() => {
+      if (displayValues.width <= 428) return 'max-width: 100%'
+      return 'max-width: 40%'
+    })
+
     return {
       responsiveCols,
-      users
+      responsiveSearchBar,
+      listedUsers,
+      users,
+      search
     };
   },
 };
 
 </script>
-
+<!-- {
+  page: number
+  itemsPerPage: number
+  pageCount: number
+  prevPage: () => void
+  nextPage: () => void
+  items: DataIteratorItem[]
+} -->
 <template>
-  <v-container>
-    <v-row class="d-flex justify-center">
-      <v-col v-for="item in users" :key="item.id" :cols=responsiveCols>
-        <v-card color="indigo" height="400">
-          <v-card-item>
-            {{ item.id }}
-            <div class="d-flex flex-column justify-space-around align-center">
-              <div class="text-center">
-                <v-card-title class="text-h5">
-                  {{ item.first_name }}
-                </v-card-title>
-                <v-card-title class="text-h5">
-                  {{ item.last_name }}
-                </v-card-title>
-                <v-card-subtitle>{{ item.email }}</v-card-subtitle>
-              </div>
+  <v-card>
+    <v-data-iterator
+      :items="listedUsers.data"
+      :items-per-page="listedUsers.per_page"
+      :search="search"
+    >
 
-              <v-avatar class="ma-3" rounded="0" size="160">
-                <v-img :src="item.avatar"></v-img>
-              </v-avatar>
-            </div>
-          </v-card-item>
-          <div class="d-flex justify-space-around">
-            <v-card-actions>
-              <v-btn class="" rounded="lg" variant="flat" color="yellow">Editar</v-btn>
-            </v-card-actions>
-            <v-card-actions>
-              <v-btn class="" rounded="lg" variant="flat" color="#f00023">Excluir</v-btn>
-            </v-card-actions>
+      <template v-slot:header>
+        <v-toolbar class="px-2">
+          <v-text-field
+            v-model="search"
+            density="comfortable"
+            placeholder="Search"
+            prepend-inner-icon="mdi-magnify"
+            :style="responsiveSearchBar"
+            variant="solo"
+            clearable
+            hide-details
+          ></v-text-field>
+        </v-toolbar>
+      </template>
+
+      <template v-slot:default={items}>
+        <v-container class="pa-2">
+          <v-row dense class="justify-center align-center" >
+            <v-col
+              v-for="item in items" :key="item.id" :cols=responsiveCols
+              md="4"
+            >
+            <v-card color="indigo" height="440">
+              <v-card-item>
+                <p class="text-h6">ID: {{ item.raw.id }}</p>
+                <div class="d-flex flex-column justify-space-around align-center">
+                  <div class="text-center">
+                    <v-card-title class="text-h4">
+                      {{ item.raw.first_name }}
+                    </v-card-title>
+                    <v-card-title class="text-h5">
+                      {{ item.raw.last_name }}
+                    </v-card-title>
+                    <v-card-subtitle>{{ item.raw.email }}</v-card-subtitle>
+                  </div>
+    
+                  <v-avatar class="ma-3" rounded="0" size="160">
+                    <v-img :src="item.raw.avatar"></v-img>
+                  </v-avatar>
+                </div>
+              </v-card-item>
+              <div class="d-flex justify-space-around">
+                <v-card-actions>
+                  <v-btn class="" rounded="lg" variant="flat" color="yellow">Editar</v-btn>
+                </v-card-actions>
+                <v-card-actions>
+                  <v-btn class="" rounded="lg" variant="flat" color="#f00023">Excluir</v-btn>
+                </v-card-actions>
+              </div>
+            </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </template>
+
+      <template v-slot:footer="{ page , pageCount, prevPage, nextPage }">
+        <div class="d-flex align-center justify-center pa-4">
+          <v-btn
+            :disabled="page === 1"
+            density="comfortable"
+            icon="mdi-arrow-left"
+            variant="tonal"
+            rounded
+            @click="prevPage"
+          ></v-btn>
+
+          <div class="mx-2 text-caption">
+            Page {{ page }} of {{ pageCount }}
           </div>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+
+          <v-btn
+            :disabled="page >= pageCount"
+            density="comfortable"
+            icon="mdi-arrow-right"
+            variant="tonal"
+            rounded
+            @click="nextPage"
+          ></v-btn>
+        </div>
+      </template>
+    </v-data-iterator>
+  </v-card>
 </template>
+
+<style lang="scss" scoped>
+
+::v-deep .v-data-iterator {
+  .v-toolbar {
+    .v-toolbar__content {
+      justify-content: center;
+      
+    }
+  }
+}
+</style>
