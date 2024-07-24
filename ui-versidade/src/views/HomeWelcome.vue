5624e9c05ff0b6b3ded1a37fd04337cb064fdda7
Carlos de Lima Junior <cj.moyses@gmail.com>
2024-07-24 10:27:26 -0300

    style: improve responsiveness and layout of home view

--- ui-versidade/src/views/HomeWelcome.vue
+++ ui-versidade/src/views/HomeWelcome.vue
@@ -10,23 +10,21 @@ export default {
 </script>
 
 <template>
-    <v-container id="welcome-container">
+    <v-container id="welcome-container" fluid>
         <v-col cols="md-4" offset="lg-4" id="welcome-box">
-            <v-card color="light" elevation="8" variant="flat" class="mx-auto text-body-1" id="welcome-card">
-                <v-row class="welcome-row" align="center" justify="center" no-gutters>
-                    <p class="text-center">SEJA BEM VINDO <br>ao<br> UI-Versidade </p>
-                    <p class="text-justify">O melhor sistema de Gerenciamento para sua Instituição Educacional.<br>
-                        Crie, edite, liste e exclua cadastros de alunos e professores em alguns segundos.</p>
-                </v-row>
-                <v-row class="welcome-row" align="center" justify="center" no-gutters>
-                    <p>Faça o Login ou seu Cadastro</p>
-                </v-row>
-                <v-row class="welcome-row" align="center" justify="space-evenly" no-gutters>
-                    <v-btn class="welcome-btn" rounded="lg" variant="tonal" size="large"
+            <v-card color="light" elevation="8" variant="flat" class="mx-auto pa-10 text-body-1 d-flex flex-column align-center justify-space-evenly" id="welcome-card">
+                <div class="d-flex flex-column align-center justify-center">
+                    <p class="text-center text-h4">Seja bem vindo ao UI-Versidade</p>
+                    <p class="text-center text-subtitle-1">O melhor sistema de Gerenciamento para sua Instituição Educacional.</p>
+                    <p class="text-center text-subtitle-1">Administre cadastros de usuários e recurso em segundos.</p>
+                    <p class="text-center text-subtitle-1">Faça o Login ou seu Cadastro</p>
+                </div>
+                <div class="d-flex justify-center align-center ga-5">
+                    <v-btn class="welcome-btn" rounded="lg" variant="flat" color="green" size="large"
                         @click="goToLoginRegisterView('login')">Login</v-btn>
-                    <v-btn class="welcome-btn" rounded="lg" variant="tonal" size="large"
+                    <v-btn class="welcome-btn" rounded="lg" variant="flat" color="blue" size="large"
                         @click="goToLoginRegisterView('register')">Cadastro</v-btn>
-                </v-row>
+                </div>
             </v-card>
         </v-col>
     </v-container>
@@ -44,8 +42,6 @@ export default {
         }
 
         .welcome-row {
-            height: 30%;
-            padding: 30px;
 
             .welcome-btn {
                 padding: 10px 20px;
