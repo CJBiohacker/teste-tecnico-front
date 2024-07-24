75d7ce068a6bb965f24269689bcec70724bcbddf
Carlos de Lima Junior <cj.moyses@gmail.com>
2024-07-24 10:26:38 -0300

    chore: append Apex Charts into application

--- ui-versidade/src/main.js
+++ ui-versidade/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import VueApexCharts from "vue3-apexcharts";
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
@@ -9,5 +10,5 @@ import './styles/base.css';
 
 const pinia = createPinia();
 const app = createApp(App);
-app.use(pinia).use(router).use(vuetify);
+app.use(pinia).use(router).use(vuetify).use(VueApexCharts);
 app.mount('#app');
