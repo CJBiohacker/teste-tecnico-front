097ed9f90ab85bd71299036739cac791f2b97cee
Carlos de Lima Junior <cj.moyses@gmail.com>
2024-07-24 21:02:44 -0300

    feat: add button to navigate backwards

--- ui-versidade/src/components/HeaderComponent.vue
+++ ui-versidade/src/components/HeaderComponent.vue
@@ -1,15 +1,22 @@
 <script>
-import { defineComponent, computed } from 'vue';
+import { defineComponent, computed, reactive } from 'vue';
 import { useThemeStore } from '@/stores/theme';
+import { useRoute, useRouter } from 'vue-router';
 
 export default defineComponent({
   setup() {
+    const route = useRoute();
+    const router = useRouter();
     const themeStore = useThemeStore();
+
+    const condition = reactive(() => [undefined, false, null, 'home'].includes(route.name));
 
     const themeName = computed(() => themeStore.theme.name);
     const appBarColor = computed(() => themeStore.theme.name === 'Claro' ? 'primary' : '');
-
     return {
+      route,
+      router,
+      condition,
       theme: themeStore.theme.color,
       name: themeName,
       toggleTheme: themeStore.toggleTheme,
@@ -21,7 +28,10 @@ export default defineComponent({
 
 <template>
   <v-app-bar scroll-behavior="elevate" :color="appBarColor">
-    <v-app-bar-title>UI-Versidade</v-app-bar-title>
+    <v-btn v-if="!condition()" @click="router.back()">
+      Voltar
+    </v-btn>
+    <v-app-bar-title class="text-center">UI-Versidade</v-app-bar-title>
     <v-switch :label="`${name}`" false-value="yes" true-value="no" hide-details color="primary"
       @click="toggleTheme"></v-switch>
   </v-app-bar>
